f6eb62737e5cd80333ca632369589c18f7fa21ab
David Whittington <djwhitt@gmail.com>
2017-02-08 11:54:50 -0600

    Moving laptop configuration into machines/

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 .PHONY: switch sync
 
+local_config:=$(shell hostname -s).nix
+
 switch: sync
 	sudo nixos-rebuild switch
 
 sync:
 	sudo rsync -r ./ /etc/nixos/
+	sudo ln -sf /etc/nixos/machines/$(local_config) /etc/nixos/configuration.nix
--- machines/djwhitt-laptop.nix
+++ machines/djwhitt-laptop.nix
@@ -7,8 +7,8 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./private/wifi.nix
+      ../hardware-configuration.nix
+      ../private/wifi.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
