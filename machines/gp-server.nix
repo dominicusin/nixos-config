8dfa47bad746d0f43e0f2348902364cf75310c25
David Whittington <djwhitt@gmail.com>
2017-02-15 14:11:12 -0600

    Import base.nix in gp-server.nix

--- machines/gp-server.nix
+++ machines/gp-server.nix
@@ -1,13 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ <nixpkgs/nixos/modules/virtualisation/amazon-image.nix> ];
+  imports =
+    [
+      <nixpkgs/nixos/modules/virtualisation/amazon-image.nix>
+      ../config/base.nix
+    ];
+
   ec2.hvm = true;
 
   networking.hostName = "gp-server";
-
-  environment.systemPackages = with pkgs; [
-    termite
-    vim
-  ];
 }
