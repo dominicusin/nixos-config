b1a54e1eadb85a54ccb4a2a32bbe8615fdafb63e
David Whittington <djwhitt@gmail.com>
2017-03-06 16:12:40 -0600

    Enable autoUpgrade for server

--- machines/gp-server.nix
+++ machines/gp-server.nix
@@ -10,15 +10,39 @@
 
   ec2.hvm = true;
 
-  networking.hostName = "gp-server";
+  system.autoUpgrade.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    boot
-    certbot
-    nodejs
-    openjdk
-  ];
+  networking.hostName = "gp-server";
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
 
   services.jenkins.enable = true;
-  services.nginx.enable = true;
+
+  security.acme.certs."jenkins.spcom.org" = {
+    email = "jenkins@spcom.org";
+  };
+
+  services.nginx = {
+    enable = true;
+
+    virtualHosts = {
+      "jenkins.spcom.org" = {
+        port = 443;
+        forceSSL = true;
+        enableACME = true;
+
+        locations = {
+          "/" = {
+            proxyPass = "http://127.0.0.1:8080";
+            extraConfig = ''
+              proxy_set_header        Host $host:$server_port;
+              proxy_set_header        X-Real-IP $remote_addr;
+              proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header        X-Forwarded-Proto $scheme;
+              proxy_redirect http:// https://;
+            '';
+          };
+        };
+      };
+    };
+  };
 }
