717426c7c5d9c04a859b139677542770142ee2c5
David Whittington <djwhitt@gmail.com>
2017-07-21 11:01:09 -0500

    Remove wiki config from ec2-gp-server

--- machines/ec2-gp-server.nix
+++ machines/ec2-gp-server.nix
@@ -134,26 +134,6 @@
           };
         };
       };
-
-     "wiki.djwhitt.com" = {
-        port = 443;
-        forceSSL = true;
-        enableACME = true;
-
-        locations = {
-          "/" = {
-            proxyPass = "http://127.0.0.1:3000";
-            extraConfig = ''
-              proxy_set_header    Host                $host:$server_port;
-              proxy_set_header    X-Real-IP           $remote_addr;
-              proxy_set_header    X-Forwarded-For     $proxy_add_x_forwarded_for;
-              proxy_set_header    X-Forwarded-Proto   $scheme;
-              proxy_set_header    X-Frame-Options     SAMEORIGIN;
-              proxy_redirect      http:// https://;
-            '';
-          };
-        };
-      };
     };
   };
 }
