e234299fd579944e9197a2eec7c3e3c510380067
David Whittington <djwhitt@gmail.com>
2018-04-14 20:20:40 -0500

    Adding deploy script

---
+++ deploy.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -e
+
+remote_hosts="home-gp-server"
+
+usage() {
+  echo "Usage"
+}
+
+while getopts "hsr:R" opt; do
+  case "$opt" in
+    h)
+      usage
+      exit 0
+      ;;
+    s)
+      sync_only=true
+      ;;
+    r)
+      sync_remote=true
+      remote_hosts="$OPTARG"
+      ;;
+    R)
+      sync_remote=true
+      ;;
+  esac
+done
+
+if [ -z "$sync_remote" ]; then
+  echo "Syncing local configuration..."
+  $HOME/Work/vs/lono-nixos-config/sync.sh
+
+  if [ -z "$sync_only" ]; then
+    echo "Switching local configuration..."
+    sudo nixos-rebuild switch --upgrade
+  fi
+fi
+
+if [ "$sync_remote" = true ]; then
+  for host in "$remote_hosts"; do
+    echo "Syncing configuration for ${host}..."
+    rsync -r ./ root@${host}:/etc/nixos/
+
+    if [ -z "$sync_only" ]; then
+      echo "Switching configuration on ${host}..."
+      ssh "root@${host}" 'bash -c "ln -sf /etc/nixos/machines/$(hostname -s).nix /etc/nixos/configuration.nix"'
+      ssh "root@${host}" nixos-rebuild switch
+    fi
+  done
+fi
